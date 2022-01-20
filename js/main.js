080dd44cf09aa6d1e11c6ea38638cb5639219c36
andruM <matseliukh.andrii@gmail.com>
2022-01-20 23:19:06 +0200

    final fixes

--- js/main.js
+++ js/main.js
@@ -41,8 +41,6 @@ const toggleMenu = function () {
   headerMenu.classList.toggle("_active");
   burgerMenu.classList.toggle("_active");
 };
-// console.log(burgerMenu);
-// console.log(headerMenu);
 
 if (burgerMenu) {
   burgerMenu.addEventListener("click", function (e) {
@@ -63,7 +61,6 @@ document.addEventListener("click", function (e) {
   }
 });
 
-
 // -------main-slider -------- Swiper
 const mainSlider = document.querySelector(".main-slider__body");
 
@@ -71,13 +68,7 @@ const mainSwiper = new Swiper(mainSlider, {
   //   // If we need pagination
   pagination: {
      el: ".swiper-pagination",
-    // el: ".slider-content__counts",
     type: "fraction",
-    // clicable: true,
-    // renderFraction: function (currentClass, totalClass) {
-    //   return '0<span class="' + currentClass + '"></span>' + '/' +
-    //   '0<span class="' + totalClass + '"></span>';
-    // },
   },
 
   // Navigation arrows
@@ -85,103 +76,7 @@ const mainSwiper = new Swiper(mainSlider, {
     nextEl: ".main-slider__body .swiper-button-next",
     prevEl: ".main-slider__body .swiper-button-prev"
   },
-
-  // Pagination fraction
-
-  //   // And if we need scrollbar
-  //   scrollbar: {
-  //     el: '.swiper-scrollbar',
-  //   },
 });
-
-// const categorySlider = document.querySelector(".category-slider");
-
-// let categorySwiper;
-
-// function mobileSlider() {
-//   if (window.innerWidth <=767 && categorySlider.dataset.mobile == 'false') {
-//     categorySwiper = new Swiper(categorySlider, {
-//       slidesPerView: 1.5,
-//       loop: true,
-//       // slideBetween: 50,
-//       pagination: {
-//         el: '.swiper-pagination',
-//         // type: 'fraction',
-//         // type: 'bullets',
-//         clickable: true,
-//         dynamicBullets: true,
-//       },
-//       breakpoints: {
-//         480: {
-//           slidesPerView: 2.5,
-//         }
-//       }
-//       // navigation: {
-//       //   nextEl: '.swiper-button-next',
-//       //   prevEl: '.swiper-button-prev',
-//       // },
-//     }); 
-
-//     categorySlider.dataset.mobile = "true";
-//   }
-
-//   if (window.innerWidth > 767) {
-//     categorySlider.dataset.mobile = "false";
-
-//     if (categorySlider.classList.contains('swiper-initialized')) {
-//       categorySwiper.destroy();
-//     }
-
-    
-//   }
-// }
-// mobileSlider();
-
-// window.addEventListener('resize', () => {
-//   mobileSlider();
-// });
-
-
-  //   if (window.innerWidth <= 767 && categorySlider.dataset.mobile == "false") {
-  //     
-        // breakpoints: {
-        //   480: {
-        //     slidesPerView: 2.5,
-        //   }
-        // }
-  //     });
-  //     categorySlider.dataset.mobile = "true";
-// }
-
-//   if (window.innerWidth > 767) {
-//     categorySlider.dataset.mobile = "false";
-
-//     // categorySlider.destroy();
-//   }
-// }
-// // mobileSlider();
-
-// window.addEventListener('resize', () => {
-//   mobileSlider();
-// });
-
-// -----------------------------------------
-
-let area = document.querySelector('.form__textarea');
-let areaPlaceholder = area.getAttribute("placeholder");
-let newAreaPlaceholder = areaPlaceholder.split('').splice(-1, 1)[0];
-
-area.hasAttribute('placeholder');
-
-// areaPlaceholder = areaPlaceholder.spliе(', ');
-
-console.log(area);
-console.log(area.hasAttribute('placeholder'));
-console.log(area.getAttribute('placeholder'));
-console.log(areaPlaceholder.split('').splice(-1, 1));
-console.log(newAreaPlaceholder);
-
-console.log(areaPlaceholder);
 
 // ------------------------------------------
 
